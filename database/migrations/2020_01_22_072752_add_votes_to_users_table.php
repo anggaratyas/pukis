5015ea2111675782166f435180d8c0fcb831be72
bobsin <anggaratyas15@gmail.com>
2020-01-22 15:31:15 +0700

    menambah fitur soft delete pada users table

---
+++ database/migrations/2020_01_22_072752_add_votes_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddVotesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('photo')->nullable();
+            $table->string('api_token')->nullable();
+            $table->string('role', 1)->comment('0: sudo, 1: admin, 2: pemdes, 3: bumdes, 4: pegawai, 5: member, 6: ');
+            $table->unsignedBigInteger('otlet_id')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
